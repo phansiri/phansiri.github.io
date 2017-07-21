dac8eaa217755b1fefea5b74dcb8e93c52707f05
Lit Phansiri <phansiri@uw.edu>
2017-07-21 13:26:55 -0700

    incorpate scrollspy for spa

--- js/init.js
+++ js/init.js
@@ -9,6 +9,7 @@
     });
     $('.parallax').parallax();
     $('.collapsible').collapsible();
+    $('.scrollspy').scrollSpy();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
